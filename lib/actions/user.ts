9fbcc91ce5ce9e8a7cf83d48e2a5cf02ceb38632
akhilraj960 <akhilraj5010@gmail.com>
2024-06-24 14:04:49 +0530

    clerk check 3

--- lib/actions/user.ts
+++ lib/actions/user.ts
@@ -33,13 +33,28 @@ export const createUser = async ({
       },
     });
 
-    await user.save().then((user) => {
-      console.log("user created", user);
+    const newUser = await user.save();
 
-      return user;
-    });
+    console.log("new User Created ", newUser);
+
+    // const user = await new User({
+    //   clerkId,
+    //   personal_info: {
+    //     firstName: first_name,
+    //     lastName: last_name,
+    //     profile_img: image_url,
+    //     email: email_addresses[0].email_address,
+    //     username: username,
+    //   },
+    // });
+
+    // await user.save().then((user) => {
+    //   console.log("user created", user);
+
+    //   return user;
+    // });
   } catch (error) {
-    console.log(error);
+    console.log("Error : ", error);
   }
 };
 
